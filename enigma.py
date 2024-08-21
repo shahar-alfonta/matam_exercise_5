dcf79a39fbf8c36a18804b116a4e8be79bf6d9c5
alfonta-datetime <shahar.alfonta@gmail.com>
2024-08-21 14:22:33 +0300

    adds load_enigma_from_path

--- enigma.py
+++ enigma.py
@@ -1,3 +1,8 @@
+import json
+
+class JSONFileError(Exception):
+    pass
+
 def encrypt_letter(self, letter):
     if c.isupper():
         i = hash_map[c]
@@ -16,6 +21,7 @@ def encrypt_letter(self, letter):
         c3 = self.hash_map[i]
         return c3
 
+
 class Enigma:
     def __init__(self, hash_map, wheels, reflector_map):
         self.hash_map = hash_map
@@ -23,24 +29,35 @@ class Enigma:
         self.reflector_map = reflector_map
         self.new_message
 
-
     def encrypt(self, message):
         wheels = self.wheels
         for c in message:
-                self.new_message += encrypt_letter(c)
-            wheels[0] = wheels[0] % 8 +1
-            if sum(1 for letter in message if letter.islower()) % 2 == 0:
-                wheels[1] *= 2
+            self.new_message += encrypt_letter(c)
+        wheels[0] = wheels[0] % 8 + 1
+        if sum(1 for letter in message if letter.islower()) % 2 == 0:
+            wheels[1] *= 2
+        else:
+            wheels[1] -= 1
+        if sum(1 for letter in message if letter.islower()) % 10 == 0:
+            wheels[2] = 10
+        elif sum(1 for letter in message if letter.islower()) % 10 == 0:
+            wheels[2] = 5
             else:
-                wheels[1]-=1
-            if sum(1 for letter in message if letter.islower()) % 10 == 0:
-                wheels[2] = 10
-            elif sum(1 for letter in message if letter.islower()) % 10 == 0:
-                wheels[2] = 5
-                else:
-                    wheels[2] = 0
-        return self.new_message
+            wheels[2] = 0
+
+    return self.new_message
 
 
 def load_enigma_from_path(path):
-    pass
+    try:
+        with open(path, 'r') as conf_file:
+            conf_dict = json.load(conf_file)
+    except:
+        raise JSONFileError
+
+    return Enigma(
+        conf_dict['hash_map'],
+        conf_dict['wheels'],
+        conf_dict['reflector_map']
+    )
+
