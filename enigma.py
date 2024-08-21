04bb91d9f5cf8dafd549bfeb3d6a24a5f4a75ae4
nehoray-levy <148368002+nehoray-levy@users.noreply.github.com>
2024-08-21 14:23:05 +0300

    fixed anigma class

--- enigma.py
+++ enigma.py
@@ -1,20 +1,5 @@
-def encrypt_letter(self, letter):
-    if c.isupper():
-        i = hash_map[c]
-        if ((wheels[0] * 2 - wheels[2] + wheels[3]) % 26 != 0):
-            i += (wheels[0] * 2 - wheels[2] + wheels[3]) % 26
-        else:
-            i += 1
-        i = i % 26
-        c1 = self.hash_map[i]
-        c2 = self.reflector_map[c21]
-        i = self.hash_map[c2]
-        if ((wheels[0] * 2 - wheels[2] + wheels[3]) % 26 != 0):
-            i -= (wheels[0] * 2 - wheels[2] + wheels[3]) % 26
-        else:
-            i -= 1
-        c3 = self.hash_map[i]
-        return c3
+ALPHABET_LENGTH = 26
+
 
 class Enigma:
     def __init__(self, hash_map, wheels, reflector_map):
@@ -23,22 +8,46 @@ class Enigma:
         self.reflector_map = reflector_map
         self.new_message
 
+    def move_wheels (self, wheels , len):
+        wheels[0] = wheels[0] % 8 + 1
+        if len % 2 == 0:
+            wheels[1] *= 2
+        else:
+            wheels[1] -= 1
+        if len % 10 == 0:
+            wheels[2] = 10
+        elif len % 3 == 0:
+            wheels[2] = 5
+            else:
+            wheels[2] = 0
+
+    def encrypt_letter(self, letter):
+        if letter.isupper():
+            i = hash_map[letter]
+            if ((wheels[0] * 2 - wheels[1] + wheels[2]) % ALPHABET_LENGTH != 0):
+                i += (wheels[0] * 2 - wheels[1] + wheels[2]) % ALPHABET_LENGTH
+            else:
+                i += 1
+            i = i % ALPHABET_LENGTH
+            c1 = self.hash_map[i]
+            c2 = self.reflector_map[c21]
+            i = self.hash_map[c2]
+            if ((wheels[0] * 2 - wheels[1] + wheels[2]) % ALPHABET_LENGTH != 0):
+                i -= (wheels[0] * 2 - wheels[1] + wheels[2]) % ALPHABET_LENGTH
+            else:
+                i -= 1
+            c3 = self.hash_map[i]
+            return c3
+        else:
+            return letter
 
     def encrypt(self, message):
         wheels = self.wheels
+        len = 0
         for c in message:
-                self.new_message += encrypt_letter(c)
-            wheels[0] = wheels[0] % 8 +1
-            if sum(1 for letter in message if letter.islower()) % 2 == 0:
-                wheels[1] *= 2
-            else:
-                wheels[1]-=1
-            if sum(1 for letter in message if letter.islower()) % 10 == 0:
-                wheels[2] = 10
-            elif sum(1 for letter in message if letter.islower()) % 10 == 0:
-                wheels[2] = 5
-                else:
-                    wheels[2] = 0
+            len +=1
+            self.new_message += encrypt_letter(c)
+        move_wheels(wheels, len)
         return self.new_message
 
 
